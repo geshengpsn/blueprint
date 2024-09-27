eefa736129a01dcc20423b06d2064868e53579f5
geshengpsn <gesheng199832@126.com>
2024-09-27 20:56:13 +0800

    add record data + send buttons & notify

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 import { ref, onMounted } from "vue";
 const rerun = ref<HTMLElement | null>(null);
 const ws = ref<WebSocket | null>(null);
+const ws1 = ref<WebSocket | null>(null);
 onMounted(() => {
   if (rerun.value) {
     const rrdUrl = "ws://localhost:4321";
@@ -18,6 +19,17 @@ onMounted(() => {
       console.log(event.data);
     };
     ws.value.onclose = () => {
+      console.log("disconnected");
+    };
+
+    ws1.value = new WebSocket("ws://localhost:4323");
+    ws1.value.onopen = () => {
+      console.log("connected");
+    };
+    ws1.value.onmessage = (event) => {
+      console.log(event.data);
+    };
+    ws1.value.onclose = () => {
       console.log("disconnected");
     };
   }
@@ -63,17 +75,40 @@ const handle_start = () => {
   console.log(nodes.value);
   if (ws.value) {
     ws.value.send(JSON.stringify(nodes.value));
+    notify.value = "Operation Done!";
+  } else {
+    notify.value = "No Connection!";
   }
 };
+
+const handle_record = () => {
+  console.log(nodes.value);
+  if (ws1.value) {
+    ws1.value.send("1");
+    notify.value = "Operation Done!";
+  } else {
+    notify.value = "No Connection!";
+  }
+};
+const notify = ref("");
 </script>
 
 <template>
-  <div class="flex" data-theme="dark" >
+  <div class="flex" data-theme="dark">
     <div ref="rerun"></div>
     <div class=" overflow-auto flex-1" style="height: 100vh;">
-      
+      <div class="flex justify-between">
+        <button class="btn btn-primary btn-lg m-4" @click="handle_record()">Record Data</button>
+        <button class="btn btn-primary btn-lg m-4" @click="handle_start()">Send</button>
+      </div>
+      <div class=" divider"></div>
       <div class=" flex flex-col justify-center items-center " style="">
-        <button class="btn btn-primary btn-lg m-4" @click="handle_start()">Start</button>
+        <!-- <button class="btn btn-primary btn-lg m-4" @click="handle_start()">Start</button> -->
+        <div class="bg-neutral rounded-2xl shadow-xl p-4 m-4">
+          <div>
+            <span class=" text-xl font-bold">start</span>
+          </div>
+        </div>
 
         <TransitionGroup name="list">
           <div v-for="(node, i) in nodes" :key="node.id" class="flex flex-col items-center">
@@ -113,12 +148,12 @@ const handle_start = () => {
             </svg>
           </div>
           <ul tabIndex={node} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-            <li @click="nodes.push({node: 0, id: nanoid()})"><a>Find Board & Press Button</a></li>
-            <li @click="nodes.push({node: 1, id: nanoid()})"><a>Move Slider to Setpoints on Screen</a></li>
-            <li @click="nodes.push({node: 2, id: nanoid()})"><a>Plug in Probe into Test Port</a></li>
-            <li @click="nodes.push({node: 3, id: nanoid()})"><a>Open Door, Probe Circuit</a></li>
-            <li @click="nodes.push({node: 4, id: nanoid()})"><a>Wrap Cable, Replace Probe</a></li>
-            <li @click="nodes.push({node: 5, id: nanoid()})"><a>Press Stop Trial Button</a></li>
+            <li @click="nodes.push({ node: 0, id: nanoid() })"><a>Find Board & Press Button</a></li>
+            <li @click="nodes.push({ node: 1, id: nanoid() })"><a>Move Slider to Setpoints on Screen</a></li>
+            <li @click="nodes.push({ node: 2, id: nanoid() })"><a>Plug in Probe into Test Port</a></li>
+            <li @click="nodes.push({ node: 3, id: nanoid() })"><a>Open Door, Probe Circuit</a></li>
+            <li @click="nodes.push({ node: 4, id: nanoid() })"><a>Wrap Cable, Replace Probe</a></li>
+            <li @click="nodes.push({ node: 5, id: nanoid() })"><a>Press Stop Trial Button</a></li>
           </ul>
         </div>
         <div class="bg-neutral rounded-2xl shadow-xl p-4 m-4">
@@ -130,10 +165,22 @@ const handle_start = () => {
     </div>
 
   </div>
+  <div v-if="notify.length !== 0" class="fixed bottom-0 right-0 m-4">
+    <div class="card w-96 shadow-xl bg-success">
+        <div class="card-body">
+          <h2 class="card-title text-success-content">Operation Done!</h2>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary text-success-content" @click="notify = ''">OK</button>
+          </div>
+        </div>
+      </div>
+
+  </div>
 </template>
 
 <style scoped>
-.list-move, /* 对移动中的元素应用的过渡 */
+.list-move,
+/* 对移动中的元素应用的过渡 */
 .list-enter-active,
 .list-leave-active {
   transition: all 0.5s ease;
